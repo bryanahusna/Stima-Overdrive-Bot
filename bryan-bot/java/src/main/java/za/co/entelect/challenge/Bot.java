89e51893b9d6ce556ed2f321a0940312a86a19ba
Maria Khelli <13520115@std.stei.itb.ac.id>
2022-02-07 23:20:19 +0700

    add truck

--- bryan-bot/java/src/main/java/za/co/entelect/challenge/Bot.java
+++ bryan-bot/java/src/main/java/za/co/entelect/challenge/Bot.java
@@ -1,15 +1,18 @@
 package za.co.entelect.challenge;
 
 import za.co.entelect.challenge.command.*;
-import za.co.entelect.challenge.entities.*;
+import za.co.entelect.challenge.entities.Car;
+import za.co.entelect.challenge.entities.GameState;
+import za.co.entelect.challenge.entities.Lane;
 import za.co.entelect.challenge.enums.PowerUps;
 import za.co.entelect.challenge.enums.Terrain;
 
-import java.util.*;
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 import static java.lang.Math.max;
-
-import java.security.SecureRandom;
 
 public class Bot {
 
@@ -37,56 +40,65 @@ public class Bot {
     public Command run(GameState gameState) {
         Car myCar = gameState.player;
         Car opponent = gameState.opponent;
+        int currentSpeedLimit = getCurrentSpeedLimit(gameState);
 
-        //Basic fix logic
-        List<Object> blocks = getBlocksInFront(myCar.position.lane, myCar.position.block, gameState);
-        List<Object> nextBlocks = blocks.subList(0,1);
-
-        //Fix first if too damaged to move
-        if(myCar.damage == 5) {
-            return FIX;
-        }
-        //Accelerate first if going to slow
-        if(myCar.speed <= 3) {
-            return ACCELERATE;
-        }
-
-        //Basic fix logic
-        if(myCar.damage >= 5) {
-            return FIX;
-        }
-
-        //Basic avoidance logic
-        if (blocks.contains(Terrain.MUD) || nextBlocks.contains(Terrain.WALL)) {
-            if (hasPowerUp(PowerUps.LIZARD, myCar.powerups)) {
-                return LIZARD;
-            }
-            if (nextBlocks.contains(Terrain.MUD) || nextBlocks.contains(Terrain.WALL)) {
-                int i = random.nextInt(directionList.size());
-                return directionList.get(i);
+        if (hasPowerUp(PowerUps.EMP, myCar.powerups)) {
+            if (Math.abs(myCar.position.lane - opponent.position.lane) <= 1 && opponent.position.block > myCar.position.block) {
+                return EMP;
             }
         }
 
-        //Basic improvement logic
+        // Avoidance logic
+        List<Terrain> frontBlocks = getBlocksFront(myCar, gameState, 1);
+        List<Terrain> leftBlocks = getBlocksLeft(myCar, gameState, 1);
+        List<Terrain> rightBlocks = getBlocksRight(myCar, gameState, 1);
+
+        int frontObstacleScore = calculateObstacleScore(frontBlocks, 2, 4);
+        int leftObstacleScore = calculateObstacleScore(leftBlocks, 2, 4);
+        int rightObstacleScore = calculateObstacleScore(rightBlocks, 2, 4);
+
+
+        if (hasPowerUp(PowerUps.LIZARD, myCar.powerups)) {
+            frontObstacleScore = 0;
+            return LIZARD;
+        }
+        if (frontObstacleScore > 0 && myCar.speed > 0) {
+            if (frontObstacleScore <= leftObstacleScore && frontObstacleScore <= rightObstacleScore) {
+                return ACCELERATE;
+            } else if (leftObstacleScore < frontObstacleScore && leftObstacleScore <= rightObstacleScore) {
+                return TURN_LEFT;
+            } else {
+                return TURN_RIGHT;
+            }
+        }
+
+        if (myCar.speed < currentSpeedLimit) {
+            if (hasPowerUp(PowerUps.BOOST, myCar.powerups)) {
+                if (myCar.damage > 0) {
+                    return FIX;
+                } else {
+                    return BOOST;
+                }
+            } else {
+                return ACCELERATE;
+            }
+        }
+
+        if (myCar.damage > 0) {
+            return FIX;
+        }
         if (hasPowerUp(PowerUps.BOOST, myCar.powerups)) {
             return BOOST;
         }
-
-        //Basic aggression logic
-        if (myCar.speed == maxSpeed) {
-            if (hasPowerUp(PowerUps.OIL, myCar.powerups)) {
-                return OIL;
-            }
-            if (hasPowerUp(PowerUps.EMP, myCar.powerups)) {
-                return EMP;
-            }
+        if (hasPowerUp(PowerUps.OIL, myCar.powerups) && opponent.position.block < myCar.position.block) {
+            return OIL;
         }
 
         return ACCELERATE;
     }
 
     private Boolean hasPowerUp(PowerUps powerUpToCheck, PowerUps[] available) {
-        for (PowerUps powerUp: available) {
+        for (PowerUps powerUp : available) {
             if (powerUp.equals(powerUpToCheck)) {
                 return true;
             }
@@ -94,13 +106,73 @@ public class Bot {
         return false;
     }
 
+    private int calculateObstacleScore(List<Terrain> terrains, int mudScore, int wallScore) {
+        int score = 0;
+        if (terrains.size() == 0) {
+            return 9999;
+        }
+        for (Terrain terrain : terrains) {
+            if (terrain == Terrain.MUD || terrain == Terrain.OIL_SPILL) {
+                score += mudScore;
+            } else if (terrain == Terrain.WALL) {
+                score += wallScore;
+            } else if (terrain == Terrain.CYBERTRUCK) {
+                score += 100;
+            }
+        }
+        return score;
+    }
+
+    /* Mengambil daftar terrain block-block di depan, kanan, dan kiri mobil
+     *  Mengambil sebanyak speed * speedMultiplier block
+     *  Untuk getBlocksFront, block diambil mulai DARI DEPAN MOBIL (block mobil + 1).
+     *  getBlocksLeft dan Right dimulai SEJAJAR DENGAN MOBIL (berguna mengecek pertimbangan turn left/right)
+     *  Return List kosong jika arah tidak valid (misal cek block left jika mobil di lane 1) */
+    private List<Terrain> getBlocksFront(Car myCar, GameState gameState, int speedMultiplier) {
+        return getBlocks(myCar.position.lane, myCar.position.block + 1, speedMultiplier * myCar.speed, gameState);
+    }
+
+    private List<Terrain> getBlocksLeft(Car myCar, GameState gameState, int speedMultiplier) {
+        if (myCar.position.lane == 1) {
+            return new ArrayList<>();
+        } else {
+            return getBlocks(myCar.position.lane - 1, myCar.position.block, speedMultiplier * myCar.speed, gameState);
+        }
+    }
+
+    private List<Terrain> getBlocksRight(Car myCar, GameState gameState, int speedMultiplier) {
+        if (myCar.position.lane == 4) {
+            return new ArrayList<>();
+        } else {
+            return getBlocks(myCar.position.lane + 1, myCar.position.block, speedMultiplier * myCar.speed, gameState);
+        }
+    }
+
+    // lane dan block adalah posisi absolut, bukan indeks array
+    private List<Terrain> getBlocks(int lane, int startBlock, int length, GameState gameState) {
+        Lane[] laneArr = gameState.lanes.get(lane - 1);
+        List<Terrain> blocks = new ArrayList<>();
+
+        int startIdx = startBlock - laneArr[0].position.block;
+        int endIdx = startIdx + length - 1;
+        for (int i = startIdx; i <= endIdx; i++) {
+            if (laneArr[i] == null || laneArr[i].terrain == Terrain.FINISH) {
+                break;
+            }
+
+            blocks.add(laneArr[i].terrain);
+        }
+        return blocks;
+    }
+
     /**
      * Returns map of blocks and the objects in the for the current lanes, returns
      * the amount of blocks that can be traversed at max speed.
-     **/
-    private List<Object> getBlocksInFront(int lane, int block, GameState gameState) {
+     */
+
+    private List<Terrain> getBlocksInFront(int lane, int block, GameState gameState) {
         List<Lane[]> map = gameState.lanes;
-        List<Object> blocks = new ArrayList<>();
+        List<Terrain> blocks = new ArrayList<>();
         int startBlock = map.get(0)[0].position.block;
 
         Lane[] laneList = map.get(lane - 1);
@@ -108,11 +180,41 @@ public class Bot {
             if (laneList[i] == null || laneList[i].terrain == Terrain.FINISH) {
                 break;
             }
-
-            blocks.add(laneList[i].terrain);
+            if (laneList[i].occupiedByCyberTruck) {
+                blocks.add(Terrain.CYBERTRUCK);
+            } else {
+                blocks.add(laneList[i].terrain);
+            }
 
         }
         return blocks;
+    }
+
+    private int getCurrentSpeedLimit(GameState gameState) {
+        int currentMaxSpeed;
+        switch (gameState.player.damage) {
+            case 0:
+                currentMaxSpeed = 15;
+                break;
+            case 1:
+                currentMaxSpeed = 9;
+                break;
+            case 2:
+                currentMaxSpeed = 8;
+                break;
+            case 3:
+                currentMaxSpeed = 6;
+                break;
+            case 4:
+                currentMaxSpeed = 3;
+                break;
+            case 5:
+                currentMaxSpeed = 0;
+                break;
+            default:
+                currentMaxSpeed = -1;
+        }
+        return currentMaxSpeed;
     }
 
 }
