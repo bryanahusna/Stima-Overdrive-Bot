c216870b51a16bc547f01a38fc7009e5c9b53b47
firizky29 <13520095@std.stei.itb.ac.id>
2022-02-16 11:31:06 +0700

    Tinggal meng-greedy yay

--- f1-bot/java/src/main/java/za/co/entelect/challenge/Bot.java
+++ f1-bot/java/src/main/java/za/co/entelect/challenge/Bot.java
@@ -25,7 +25,7 @@ import java.util.Scanner;
 public class Bot {
     private static final String ROUNDS_DIRECTORY = "rounds";
     private static final String STATE_FILE_NAME = "state.json";
-    public GlobalState globalState;
+    public GlobalState globalState, curState;
     public Queue<LogState> log;
     public GameState gameState;
     public Car opponent;
@@ -41,31 +41,32 @@ public class Bot {
     }
 
     public void takeRound(GameState gameState, Command command) {
-        GlobalState prevState = this.globalState.clone();
+        this.curState = this.globalState.clone();
 
         this.gameState = gameState;
         this.myCar = gameState.player;
         this.opponent = gameState.opponent;
         this.currentSpeedLimit = Supports.getCurrentSpeedLimit(gameState.player.damage);
         this.globalMap.updateNewRound(gameState);
-        this.globalState.player.update(gameState, 1);
-        this.globalState.enemy.update(gameState, 2);
+        this.curState.player.update(gameState, 1);
+        this.curState.enemy.update(gameState, 2);
+
+
 
         if(command != null){
             if(command instanceof TweetCommand){
-                if(prevState.isSomethingDeleted(1)){
-                    globalMap.map[prevState.pref_x1-1][prevState.pref_y1-1].deleteCybertruck();
+                if(globalState.isSomethingDeleted(1)){
+                    globalMap.map[globalState.pref_x1-1][globalState.pref_y1-1].deleteCybertruck();
                 }
-                else if(prevState.isSomethingDeleted(2)){
-                    globalMap.map[prevState.pref_x2-1][prevState.pref_y2-1].deleteCybertruck();
+                else if(globalState.isSomethingDeleted(2)){
+                    globalMap.map[globalState.pref_x2-1][globalState.pref_y2-1].deleteCybertruck();
                 }
                 globalMap.map[((TweetCommand) command).block-1][((TweetCommand) command).lane-1].setCybertruck();
-                globalState.setCyberTruck(((TweetCommand) command).block, ((TweetCommand) command).lane);
+                curState.setCyberTruck(((TweetCommand) command).block, ((TweetCommand) command).lane);
             }
         }
-
         if (command != null) {
-            LogState transition = new LogState(prevState, this.globalState, command);
+            LogState transition = new LogState(globalState, this.curState, command);
             this.log.add(transition);
             while (!this.log.isEmpty()) {
                 LogState head = this.log.peek();
@@ -85,16 +86,16 @@ public class Bot {
         switch(v){
             case 0:
             case 3:
-                Candidates = new Search(this.globalState, globalMap, 4, 0);
-                ret = Candidates.bestAction(this.globalState, globalMap, 0);
+                Candidates = new Search(this.curState, globalMap, 4, 0);
+                ret = Candidates.bestAction(this.curState, globalMap, 0);
                 break;
             case 6:
-                Candidates = new Search(this.globalState, globalMap, 3, 1);
-                ret = Candidates.bestAction(this.globalState, globalMap, 1);
+                Candidates = new Search(this.curState, globalMap, 3, 1);
+                ret = Candidates.bestAction(this.curState, globalMap, 1);
                 break;
             default:
-                Candidates = new Search(this.globalState, globalMap, 3, 2);
-                ret = Candidates.bestAction(this.globalState, globalMap, 2);
+                Candidates = new Search(this.curState, globalMap, 3, 2);
+                ret = Candidates.bestAction(this.curState, globalMap, 2);
         }
         return ret;
     }
@@ -112,150 +113,21 @@ public class Bot {
 
                 GameState gameState = gson.fromJson(state, GameState.class);
 
-
-
                 takeRound(gameState, prevCommand);
                 List<Command> commands = searchBestAction(myCar.speed);
                 Command command = commands.get(0);
                 if (Supports.isCommandEqual(command, Abilities.DO_NOTHING)) {
-                    command = Actions.bestAttack(commands, this.globalState, globalMap);
+                    command = Actions.bestAttack(commands, this.curState, globalMap);
                     if (Supports.isCommandEqual(command, Abilities.OIL)) {
                         this.globalMap.setTile(myCar.position.block, myCar.position.lane, Terrain.OIL_SPILL);
                     }
                 }
                 System.out.println(String.format("C;%d;%s", roundNumber, command.render()));
                 prevCommand = command;
-
+                this.globalState = this.curState;
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
-
-//        Command[] commands = new Command[6];
-//        commands[0] = Abilities.ACCELERATE;
-//        commands[1] = Abilities.BOOST;
-//        commands[2] = Abilities.TURN_LEFT;
-//        commands[3] = Abilities.TURN_RIGHT;
-//        commands[4] = Abilities.LIZARD;
-//        commands[5] = Abilities.FIX;
-//        Scoring bestCmds = null;
-//        float bestScore = Scoring.INVALID_COMMAND;
-//
-//        for(int i = 0; i < 6; i++){
-//            float currentScore;
-//            List<Command> cmds = new ArrayList<>();
-//            cmds.add(commands[i]);
-//            Scoring currScoring = new Scoring(cmds, gameState);
-//            currentScore = currScoring.calculate();
-//            if(currentScore > bestScore){
-//                bestScore = currentScore;
-//                bestCmds = currScoring;
-//            }
-//        }
-//
-//        for(int i = 0; i < 6; i++){
-//            for(int j = 0; j < 6; j++){
-//                float currentScore;
-//                List<Command> cmds = new ArrayList<>();
-//                cmds.add(commands[i]);
-//                cmds.add(commands[j]);
-//                Scoring currScoring = new Scoring(cmds, gameState);
-//                currentScore = currScoring.calculate();
-//                if(currentScore > bestScore){
-//                    bestScore = currentScore;
-//                    bestCmds = currScoring;
-//                }
-//            }
-//        }
-//
-//        for(int i = 0; i < 6; i++){
-//            for(int j = 0; j < 6; j++){
-//                for(int k = 0; k < 6; k++){
-//                    float currentScore;
-//                    List<Command> cmds = new ArrayList<>();
-//                    cmds.add(commands[i]);
-//                    cmds.add(commands[j]);
-//                    cmds.add(commands[k]);
-//                    Scoring currScoring = new Scoring(cmds, gameState);
-//                    currentScore = currScoring.calculate();
-//                    if(currentScore > bestScore){
-//                        bestScore = currentScore;
-//                        bestCmds = currScoring;
-//                    }
-//                }
-//            }
-//        }
-//
-//        if(bestCmds != null){
-//            return bestCmds.commands.get(0);
-//        }
-
-        // Calculate obstacle score in list [LEFT, FRONT, RIGHT]
-        
-        /*List<Integer> laneScores = Calculations.calculateLaneScores(this.gameState,
-                Supports.getAcceleratedSpeed(this.myCar.speed, this.myCar.damage));
-        int leftScore = laneScores.get(0);
-        int frontScore = laneScores.get(1);
-        int rightScore = laneScores.get(2);
-
-        if (Abilities.isEnemySameLane(this.myCar, this.opponent)) {
-            return Abilities.avoidEnemy(leftScore, rightScore);
-        }
-
-        if (this.myCar.damage >= 2
-                && this.currentSpeedLimit == this.myCar.speed) {
-            return Abilities.FIX;
-        }
-
-        if (frontScore > 0
-                && Supports.hasPowerUp(PowerUps.LIZARD, this.myCar.powerups)) {
-            return Abilities.LIZARD;
-        }
-
-        if (Supports.hasPowerUp(PowerUps.TWEET, this.myCar.powerups)) {
-            return Abilities.TWEET(this.opponent.position.lane,
-                    this.opponent.position.block + this.opponent.speed + 1);
-        }
-
-        if (Abilities.isEnemyBehind(this.myCar, this.opponent)
-                && Abilities.isEnemySameLane(this.myCar, this.opponent)
-                && Supports.hasPowerUp(PowerUps.OIL, this.myCar.powerups)) {
-            return Abilities.OIL;
-        }
-
-        if (Abilities.isEnemyInFront(this.myCar, this.opponent)
-                && Abilities.isEnemySameLane(this.myCar, this.opponent)
-                && Supports.hasPowerUp(PowerUps.EMP, this.myCar.powerups)
-                && Abilities.isAbleToSeeEnemy(this.myCar, this.opponent)) {
-            return Abilities.EMP;
-        }
-
-        if (Abilities.isEnemyInFront(this.myCar, this.opponent)
-                && Supports.hasPowerUp(PowerUps.EMP, this.myCar.powerups)
-                && Abilities.isAbleToSeeEnemy(this.myCar, this.opponent)) {
-            return Abilities.followEnemy(this.myCar, this.opponent);
-        }
-
-        if (frontScore > 0 && this.myCar.speed > 0) {
-            if (frontScore <= leftScore && frontScore <= rightScore) {
-                if (this.myCar.speed < this.currentSpeedLimit
-                        && Supports.hasPowerUp(PowerUps.BOOST, this.myCar.powerups)) {
-                    if (this.myCar.damage > 0) {
-                        return Abilities.FIX;
-                    } else {
-                        return Abilities.BOOST;
-                    }
-                }
-                return Abilities.ACCELERATE;
-            }
-
-            if (leftScore < frontScore && leftScore <= rightScore) {
-                return Abilities.TURN_LEFT;
-            } else {
-                return Abilities.TURN_RIGHT;
-            }
-        }*/
-
-        //return Abilities.ACCELERATE;
     }
 }
